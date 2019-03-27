bc84a988cbc6ae6ef4213dabf8733f1e8489b86a
Steven To <steven351906@yahoo.com.hk>
2019-03-27 12:25:35 +0800

    add sort function

--- src/components/stock/StockModelList.js
+++ src/components/stock/StockModelList.js
@@ -5,6 +5,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 
 import immutableToJsComponent from '../immutableToJsComponent';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -26,25 +27,98 @@ const classes = theme => ({
 });
 
 class StockModelList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sortColumn: null,
+      sortDirection: 'asc'
+    };
+  }
+  
+  onHeaderClick = (columnName) => {
+    this.setState((oldState) => {
+      let newSortDirection = null;
+      if (oldState.sortDirection === 'desc') {
+        newSortDirection = 'asc';
+      } else {
+        newSortDirection = 'desc';
+      }
+
+      return {
+        sortColumn: columnName,
+        sortDirection: newSortDirection
+      };
+    });
+  }
+
+  sort = (models, column, direction) => {
+    if (column === null || direction === null) {
+      return models;
+    }
+    
+    return models.sort((m1, m2) => {
+      if (m1[column] > m2[column]) {
+        return 1 * direction;
+      } else if (m1[column] < m2[column]) {
+        return -1 * direction;
+      } else {
+        return 0;
+      }
+    });
+  }
+
   render() {
+    // const { models, onModelClick, selected, onModelDetailsClick, order, orderBy } = this.props;
     const { models, onModelClick, selected, onModelDetailsClick } = this.props;
+    const {sortColumn, sortDirection} = this.state;
+
+    const modelsSorted = this.sort(
+      JSON.parse(JSON.stringify(models)),
+      sortColumn,
+      (sortDirection === 'asc') ? 1 : -1
+    );
 
     return (
       <Table className={classes.table}>
         <TableHead>
           <TableRow>
             <TableCell padding='none' style={{ width: '10%' }}></TableCell>
-            <TableCell padding='none'>Model</TableCell>
-            <TableCell padding='none' style={{ textAlign: 'center' }}>Score</TableCell>
-            <TableCell padding='none' style={{ textAlign: 'center' }}>Prediction</TableCell>
+            <TableCell padding='none'>
+              <TableSortLabel
+                active={sortColumn === 'modelName'}
+                direction={sortDirection}
+                onClick={() => this.onHeaderClick('modelName')}
+              >
+                Model
+              </TableSortLabel>
+            </TableCell>
+            <TableCell padding='none' style={{ textAlign: 'center' }}>
+              <TableSortLabel
+                active={sortColumn === 'score'}
+                direction={sortDirection}
+                onClick={() => this.onHeaderClick('score')}
+              >
+                Score
+              </TableSortLabel>
+            </TableCell>
+            <TableCell padding='none' style={{ textAlign: 'center' }}>
+              <TableSortLabel
+                active={sortColumn === 'percentageChange'}
+                direction={sortDirection}
+                onClick={() => this.onHeaderClick('percentageChange')}
+              >
+                Prediction
+              </TableSortLabel>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {models.map((model, i) => (
+          {modelsSorted.map((model, i) => (
             <TableRow key={i}>
-              <TableCell padding='none' onClick={() => onModelClick(i)}>
+              <TableCell padding='none' onClick={() => onModelClick(model.modelIndex)}>
                 <Checkbox
-                  checked={selected.indexOf(i) !== -1}
+                  checked={selected.indexOf(model.modelIndex) !== -1}
                   tabIndex={-1}
                   disableRipple
                 />
